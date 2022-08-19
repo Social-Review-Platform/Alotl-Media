3310784cbed7b22c020c1a2699d21968d23452d5
Joseph Park <joeepark@gmail.com>
2022-08-18 19:08:02 -0700

    Reverting changes from Heroku and Netlify config

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-
+// Trying to revert changes
 const app = express();
 const PORT = process.env.PORT || 3000;
 const userApiRouter = require('./routers/userApi.js');
